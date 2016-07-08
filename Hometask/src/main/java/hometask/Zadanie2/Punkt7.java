1d9ac93950ded05d3792aa9ce4de40962bf694c5
fix779 <fix779@gmail.com>
2016-07-09 02:05:28 +0300

    dopilil

--- Hometask/src/main/java/hometask/Zadanie2/Punkt7.java
+++ Hometask/src/main/java/hometask/Zadanie2/Punkt7.java
@@ -2,8 +2,7 @@ package hometask.Zadanie2;
 
 import java.util.Random; 
 import java.util.Scanner;
-import java.util.Arrays;
-
+        
 public class Punkt7 {
     public static void main (String []args){
     
@@ -22,12 +21,16 @@ public class Punkt7 {
     
     
         
-        vniz(n,arr);
         
-       // vpravo( n, arr);
+        
+       vpravo( n, arr);
        pe4at(arr);
-       // vlevo( n, arr);
-       // pe4at(arr);
+       vlevo( n, arr);
+       pe4at(arr);
+       vniz(n,arr);
+       pe4at(arr);
+       vverh(n,arr);
+       pe4at(arr);
     }
     
     
@@ -36,7 +39,7 @@ public class Punkt7 {
     public static void vpravo( int n, int arr[][]){
         System.out.println("Колличество сдвигов вправо");
         int x = scan();    
-        
+        x = x % n;
      for(int i = 0; i < arr.length; i ++){
           sdvig(x, n, arr[i]);
          for(int y = 0; y < arr[i].length; y++){
@@ -47,7 +50,8 @@ public class Punkt7 {
     // sdvig vlevo
     public static void vlevo (int n, int arr[][]){
         System.out.println("Колличество сдвигов влево");
-        int x = scan();    
+        int x = scan();  
+        x = x % n;
         x = n - x;
     for(int i = 0; i < arr.length; i ++){
         sdvig(x, n, arr[i]);
@@ -59,18 +63,21 @@ public class Punkt7 {
     // sdvig vniz
     public static void vniz (int n, int arr[][]){
         System.out.println("Колличество сдвигов вниз");
-        int x = scan();    
+        int x = scan();
+        x = x % n;
         
-     for(int i = 0; i < arr.length; i ++){
-        for(int y = 0; y < arr[i].length; y++){
-            sdvig(x, n, arr[i]);
-        }
-     }
-       
-     
+        
+        pov(x, n, arr);
     }
        
-
+    // sdvig vverh
+    public static void vverh(int n, int arr[][]){
+         System.out.println("Колличество сдвигов вверх");
+        int x = scan(); 
+        x = x % n;
+        x = n - x;
+        pov(x, n, arr);
+    }
     
     
     
@@ -103,7 +110,7 @@ public class Punkt7 {
     // algoritm sdviga
     public static void sdvig(int x, int n, int arr[]){
        
-        x = x % n;
+        
         
         int tmp[] = new int [x];
         
@@ -112,7 +119,41 @@ public class Punkt7 {
         System.arraycopy(arr, 0, arr, x, z);
         System.arraycopy(tmp, 0, arr, 0, x);
     }
-   
     
+    //sdvig vert
+    public static void sdvigV(int x, int n, int tmp1[]){
+         x = x % n;
+        
+        int tmp[] = new int [x];
+        
+        int z = tmp1.length - x;
+        System.arraycopy(tmp1, z, tmp, 0, x);
+        System.arraycopy(tmp1, 0, tmp1, x, z);
+        System.arraycopy(tmp, 0, tmp1, 0, x);
+    }
    
+    //povorot na 90 ..... Dances with a tambourine =)
+    public static void pov(int x, int n, int arr[][]){
+        int tmp1[][] = new int[n][n];
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int y = 0; y < arr[i].length; y++){
+                tmp1[y][i] = arr[i][y];
+            }
+        }
+        for(int i = 0; i < tmp1.length; i ++){
+          sdvigV(x, n, tmp1[i]);
+         for(int y = 0; y < tmp1[i].length; y++){
+        }
+     }
+         povN(n, tmp1, arr);
+    }
+    public static void povN(int n, int tmp1[][], int arr[][]){
+               
+        for(int i = 0; i < arr.length; i++){
+            for(int y = 0; y < arr[i].length; y++){
+                arr[i][y] = tmp1[y][i];
+            }
+        }
+    }
 }
